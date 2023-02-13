186cbd9a8ccace0739e5e75181d66b2fb721b83d
Pierre-Yves de Müllenheim <pydemull@uco.fr>
2023-02-13 08:50:06 +0000

    version 2.0.1

--- R/app_server.R
+++ R/app_server.R
@@ -138,7 +138,7 @@ app_server <- function(input, output, session) {
       init$file <- read_agd(system.file("extdata", "acc.agd", package = "activAnalyzer"))
       init$data <- prepare_dataset(system.file("extdata", "acc.agd", package = "activAnalyzer"))
       init$name <- system.file("extdata", "acc.agd", package = "activAnalyzer")
-      shiny::setProgress(100)
+      shiny::setProgress(1)
       })
     })
    
@@ -332,15 +332,21 @@ app_server <- function(input, output, session) {
           }
         
         # Creating reactive dataframe with wear/nonwear marks
-          df <- 
-            mark_wear_time(
-              dataset = data(),
-              to_epoch = as.numeric(input$to_epoch),
-              cts = cts, 
-              frame = input$frame_size, 
-              allowanceFrame = input$allowanceFrame_size,
-              streamFrame = input$streamFrame_size
-              ) 
+          withProgress(message = 'Please wait...', {
+          shiny::setProgress(0.1)
+            
+              df <- 
+                  mark_wear_time(
+                    dataset = data(),
+                    to_epoch = as.numeric(input$to_epoch),
+                    cts = cts, 
+                    frame = input$frame_size, 
+                    allowanceFrame = input$allowanceFrame_size,
+                    streamFrame = input$streamFrame_size
+                  ) 
+           shiny::setProgress(1)
+           
+                })
           
           return(df)
 
@@ -433,18 +439,21 @@ app_server <- function(input, output, session) {
   }, 
   width = "auto", 
   height = function(){
+    
+    n_levels = length(unique(df()$date))
+    
     height <- dplyr::case_when(
-      nlevels(as.factor(df()$date)) >= 8 ~ nlevels(as.factor(df()$date)) * 95,
-      nlevels(as.factor(df()$date)) == 7 ~ nlevels(as.factor(df()$date)) * 105,
-      nlevels(as.factor(df()$date)) == 6 ~ nlevels(as.factor(df()$date)) * 107,
-      nlevels(as.factor(df()$date)) == 5 ~ nlevels(as.factor(df()$date)) * 110,
-      nlevels(as.factor(df()$date)) == 4 ~ nlevels(as.factor(df()$date)) * 115,
-      nlevels(as.factor(df()$date)) == 3 ~ nlevels(as.factor(df()$date)) * 130,
-      nlevels(as.factor(df()$date)) == 2 ~ nlevels(as.factor(df()$date)) * 150,
-      nlevels(as.factor(df()$date)) == 1 ~ nlevels(as.factor(df()$date)) * 205
+      n_levels >= 8 ~ n_levels * 95,
+      n_levels == 7 ~ n_levels * 105,
+      n_levels == 6 ~ n_levels * 107,
+      n_levels == 5 ~ n_levels * 110,
+      n_levels == 4 ~ n_levels * 115,
+      n_levels == 3 ~ n_levels * 130,
+      n_levels == 2 ~ n_levels * 150,
+      n_levels == 1 ~ n_levels * 205
     )
-   return(height)
-    }, 
+    return(height)
+  }, 
   res = 120)
   
   #####################################################
@@ -1370,6 +1379,8 @@ app_server <- function(input, output, session) {
       # Building the list  
         results_list <- eventReactive(input$Run, {
           
+          withProgress(message = 'Please wait...', {
+          
             # Waiting for required conditions 
               req(
                 # Patient's characteristics
@@ -1509,7 +1520,8 @@ app_server <- function(input, output, session) {
                 period_info_15$corr_mets() >= 0,
                 )
        
-
+   
+    
             # Building the dataframe with intensity marks
               df_with_computed_metrics <-
                 df() %>%
@@ -1524,6 +1536,8 @@ app_server <- function(input, output, session) {
                   sex = input$sex,
                   dates = input$selected_days
                   )
+              
+             shiny::setProgress(0.5)  # set progress to 50%
               
             # Modifying the dataset based on the PA periods reported by the user if any
              
@@ -1644,6 +1658,8 @@ app_server <- function(input, output, session) {
                       df_with_computed_metrics$bout <- cumsum(c(1, as.numeric(diff(df_with_computed_metrics$intensity_category_num))!= 0))
               }
               
+              
+              
              # Creating a list of the results by day and corresponding to valid wear time only  
                results_by_day <-
                  df_with_computed_metrics %>%
@@ -1658,7 +1674,8 @@ app_server <- function(input, output, session) {
                    bin_width = input$bin_width
                  )
     
-     
+           shiny::setProgress(1)  # set progress to 100%
+               
              # Returning the list of the results and chosen parameters
                return(
                  list(
@@ -1671,6 +1688,7 @@ app_server <- function(input, output, session) {
                    vpa_cutpoint_chosen = cut_points()$vpa_cutpoint_chosen
                    )
                  )
+          })
     })
  
       
@@ -1738,8 +1756,6 @@ app_server <- function(input, output, session) {
   # Showing section title for activity volume metrics
     output$title_activity_volume_metrics <- renderUI({
       
-      # Waiting for needed data
-      req(nrow(results_list()$df_with_computed_metrics) >=1)
       
       # Showing title
       h3("Activity volume metrics")
@@ -1779,15 +1795,18 @@ app_server <- function(input, output, session) {
     }, 
     width = "auto", 
     height = function(){
+      
+      n_levels = length(unique(results_list()$df_with_computed_metrics$date))
+      
       height <- dplyr::case_when(
-        nlevels(as.factor(results_list()$df_with_computed_metrics$date)) >= 8 ~ nlevels(as.factor(results_list()$df_with_computed_metrics$date)) * 95,
-        nlevels(as.factor(results_list()$df_with_computed_metrics$date)) == 7 ~ nlevels(as.factor(results_list()$df_with_computed_metrics$date)) * 105,
-        nlevels(as.factor(results_list()$df_with_computed_metrics$date)) == 6 ~ nlevels(as.factor(results_list()$df_with_computed_metrics$date)) * 107,
-        nlevels(as.factor(results_list()$df_with_computed_metrics$date)) == 5 ~ nlevels(as.factor(results_list()$df_with_computed_metrics$date)) * 110,
-        nlevels(as.factor(results_list()$df_with_computed_metrics$date)) == 4 ~ nlevels(as.factor(results_list()$df_with_computed_metrics$date)) * 115,
-        nlevels(as.factor(results_list()$df_with_computed_metrics$date)) == 3 ~ nlevels(as.factor(results_list()$df_with_computed_metrics$date)) * 130,
-        nlevels(as.factor(results_list()$df_with_computed_metrics$date)) == 2 ~ nlevels(as.factor(results_list()$df_with_computed_metrics$date)) * 150,
-        nlevels(as.factor(results_list()$df_with_computed_metrics$date)) == 1 ~ nlevels(as.factor(results_list()$df_with_computed_metrics$date)) * 205
+        n_levels >= 8 ~ n_levels * 95,
+        n_levels == 7 ~ n_levels * 105,
+        n_levels == 6 ~ n_levels * 107,
+        n_levels == 5 ~ n_levels * 110,
+        n_levels == 4 ~ n_levels * 115,
+        n_levels == 3 ~ n_levels * 130,
+        n_levels == 2 ~ n_levels * 150,
+        n_levels == 1 ~ n_levels * 205
       )
       return(height)
     }, 
@@ -1926,9 +1945,6 @@ app_server <- function(input, output, session) {
   # Showing section title for intstep accumulation metrics
   output$title_step_acc_metrics <- renderUI({
     
-    # Waiting for needed data
-    req(nrow(results_list()$df_with_computed_metrics) >=1)
-    
     # Showing title
     h3("Step accumulation metrics")
   }) 
@@ -1986,9 +2002,6 @@ app_server <- function(input, output, session) {
   # Showing section title for intensity ditribution metrics
   output$title_int_distri_metrics <- renderUI({
     
-  # Waiting for needed data
-  req(nrow(results_list()$df_with_computed_metrics) >=1)
-  
   # Showing title
   h3("Intensity distribution metrics")
   }) 
@@ -2131,39 +2144,42 @@ app_server <- function(input, output, session) {
        }, 
        width = "auto", 
        height = function(){
+         
+         n_levels = length(unique(metrics_accum_sed()$recap_bouts_by_day$date))
+         
          height <- dplyr::case_when(
-           nlevels(as.factor(metrics_accum_sed()$recap_bouts_by_day$date)) >= 8 ~ nlevels(as.factor(metrics_accum_sed()$recap_bouts_by_day$date)) * 95,
-           nlevels(as.factor(metrics_accum_sed()$recap_bouts_by_day$date)) == 7 ~ nlevels(as.factor(metrics_accum_sed()$recap_bouts_by_day$date)) * 105,
-           nlevels(as.factor(metrics_accum_sed()$recap_bouts_by_day$date)) == 6 ~ nlevels(as.factor(metrics_accum_sed()$recap_bouts_by_day$date)) * 107,
-           nlevels(as.factor(metrics_accum_sed()$recap_bouts_by_day$date)) == 5 ~ nlevels(as.factor(metrics_accum_sed()$recap_bouts_by_day$date)) * 110,
-           nlevels(as.factor(metrics_accum_sed()$recap_bouts_by_day$date)) == 4 ~ nlevels(as.factor(metrics_accum_sed()$recap_bouts_by_day$date)) * 115,
-           nlevels(as.factor(metrics_accum_sed()$recap_bouts_by_day$date)) == 3 ~ nlevels(as.factor(metrics_accum_sed()$recap_bouts_by_day$date)) * 130,
-           nlevels(as.factor(metrics_accum_sed()$recap_bouts_by_day$date)) == 2 ~ nlevels(as.factor(metrics_accum_sed()$recap_bouts_by_day$date)) * 150,
-           nlevels(as.factor(metrics_accum_sed()$recap_bouts_by_day$date)) == 1 ~ nlevels(as.factor(metrics_accum_sed()$recap_bouts_by_day$date)) * 205
+           n_levels >= 8 ~ n_levels * 95,
+           n_levels == 7 ~ n_levels * 105,
+           n_levels == 6 ~ n_levels * 107,
+           n_levels == 5 ~ n_levels * 110,
+           n_levels == 4 ~ n_levels * 115,
+           n_levels == 3 ~ n_levels * 130,
+           n_levels == 2 ~ n_levels * 150,
+           n_levels == 1 ~ n_levels * 205
          )
          return(height)
-       }, res = 120
-       )
+       }, 
+       res = 120)
   
   # Plotting alpha for sedentary bouts
     output$graph_alpha_SB <- renderPlot({
       metrics_accum_sed()$p_alpha
-    }, width = "auto", height = 500, res = 100)
+    }, width = "auto", height = 400, res = 80)
     
   # Plotting MBD for sedentary bouts
      output$graph_mbd_SB <- renderPlot({
        metrics_accum_sed()$p_MBD
-     }, width = "auto", height = 500, res = 100)
+     }, width = "auto", height = 400, res = 80)
      
   # Plotting UBD for sedentary bouts
     output$graph_ubd_SB <- renderPlot({
       metrics_accum_sed()$p_UBD
-    }, width = "auto", height = 500, res = 100)
+    }, width = "auto", height = 400, res = 80)
   
   # Plotting Gini index for sedentary bouts
     output$graph_gini_SB <- renderPlot({
       metrics_accum_sed()$p_gini
-    }, width = "auto", height = 500, res = 100)
+    }, width = "auto", height = 400, res = 80)
   
     
   # Physical activity accumulation metrics panels =======================================================================
@@ -2213,39 +2229,42 @@ app_server <- function(input, output, session) {
     }, 
     width = "auto", 
     height = function(){
+      
+      n_levels = length(unique(metrics_accum_pa()$recap_bouts_by_day$date))
+      
       height <- dplyr::case_when(
-        nlevels(as.factor(metrics_accum_pa()$recap_bouts_by_day$date)) >= 8 ~ nlevels(as.factor(metrics_accum_pa()$recap_bouts_by_day$date)) * 95,
-        nlevels(as.factor(metrics_accum_pa()$recap_bouts_by_day$date)) == 7 ~ nlevels(as.factor(metrics_accum_pa()$recap_bouts_by_day$date)) * 105,
-        nlevels(as.factor(metrics_accum_pa()$recap_bouts_by_day$date)) == 6 ~ nlevels(as.factor(metrics_accum_pa()$recap_bouts_by_day$date)) * 107,
-        nlevels(as.factor(metrics_accum_pa()$recap_bouts_by_day$date)) == 5 ~ nlevels(as.factor(metrics_accum_pa()$recap_bouts_by_day$date)) * 110,
-        nlevels(as.factor(metrics_accum_pa()$recap_bouts_by_day$date)) == 4 ~ nlevels(as.factor(metrics_accum_pa()$recap_bouts_by_day$date)) * 115,
-        nlevels(as.factor(metrics_accum_pa()$recap_bouts_by_day$date)) == 3 ~ nlevels(as.factor(metrics_accum_pa()$recap_bouts_by_day$date)) * 130,
-        nlevels(as.factor(metrics_accum_pa()$recap_bouts_by_day$date)) == 2 ~ nlevels(as.factor(metrics_accum_pa()$recap_bouts_by_day$date)) * 150,
-        nlevels(as.factor(metrics_accum_pa()$recap_bouts_by_day$date)) == 1 ~ nlevels(as.factor(metrics_accum_pa()$recap_bouts_by_day$date)) * 205
+        n_levels >= 8 ~ n_levels * 95,
+        n_levels == 7 ~ n_levels * 105,
+        n_levels == 6 ~ n_levels * 107,
+        n_levels == 5 ~ n_levels * 110,
+        n_levels == 4 ~ n_levels * 115,
+        n_levels == 3 ~ n_levels * 130,
+        n_levels == 2 ~ n_levels * 150,
+        n_levels == 1 ~ n_levels * 205
       )
       return(height)
-    }, res = 120
-    )
+    }, 
+    res = 120)
   
   # Plotting alpha for  physical activity bouts
     output$graph_alpha_PA <- renderPlot({
       metrics_accum_pa()$p_alpha
-    }, width = "auto", height = 500, res = 100)
+    }, width = "auto", height = 400, res = 80)
   
   # Plotting MBD for  physical activity bouts
     output$graph_mbd_PA <- renderPlot({
       metrics_accum_pa()$p_MBD
-    }, width = "auto", height = 500, res = 100)
+    }, width = "auto", height = 400, res = 80)
     
   # Plotting UBD for  physical activity bouts
     output$graph_ubd_PA <- renderPlot({
       metrics_accum_pa()$p_UBD
-    }, width = "auto", height = 500, res = 100)
+    }, width = "auto", height = 400, res = 80)
   
   # Plotting Gini index for  physical activity bouts
     output$graph_gini_PA <- renderPlot({
       metrics_accum_pa()$p_gini
-    }, width = "auto", height = 500, res = 100)
+    }, width = "auto", height = 400, res = 80)
   
   
   #########################################
@@ -5559,21 +5578,17 @@ app_server <- function(input, output, session) {
     shinyjs::hide("update_graphic2")
     shinyjs::hide("graph_int")
     shinyjs::hide("BoxResByDayVolTab")
-    shinyjs::hide("BoxResByDayVolFig")
     shinyjs::hide("BoxResVolMeans")
     shinyjs::hide("BoxResVolMedians")
-    shinyjs::hide("BoxCompaNormsFig")
     shinyjs::hide("BoxResByDayStepTab")
-    shinyjs::hide("BoxResByDayStepFig")
     shinyjs::hide("BoxResStepMeans")
     shinyjs::hide("BoxResStepMedians")
     shinyjs::hide("BoxResByDayIntDistTab")
-    shinyjs::hide("BoxResByDayIntDistFig1")
-    shinyjs::hide("BoxResByDayIntDistFig1Bis")
-    shinyjs::hide("BoxResByDayIntDistFig2")
-    shinyjs::hide("BoxSummaryIntDistFig")
     shinyjs::hide("BoxResIntDistMeans")
     shinyjs::hide("BoxResIntDistMedians")
+    shinyjs::hide("title_activity_volume_metrics")
+    shinyjs::hide("title_step_acc_metrics")
+    shinyjs::hide("title_int_distri_metrics")
     observe({
       if(nrow(results_list()$df_with_computed_metrics) >=1) {
       shinyjs::show("myBox2")
@@ -5583,21 +5598,17 @@ app_server <- function(input, output, session) {
       shinyjs::show("update_graphic2")
       shinyjs::show("graph_int")
       shinyjs::show("BoxResByDayVolTab")
-      shinyjs::show("BoxResByDayVolFig")
       shinyjs::show("BoxResVolMeans")
       shinyjs::show("BoxResVolMedians")
-      shinyjs::show("BoxCompaNormsFig")
       shinyjs::show("BoxResByDayStepTab")
-      shinyjs::show("BoxResByDayStepFig")
       shinyjs::show("BoxResStepMeans")
       shinyjs::show("BoxResStepMedians")
       shinyjs::show("BoxResByDayIntDistTab")
-      shinyjs::show("BoxResByDayIntDistFig1")
-      shinyjs::show("BoxResByDayIntDistFig1Bis")
-      shinyjs::show("BoxResByDayIntDistFig2")
-      shinyjs::show("BoxSummaryIntDistFig")
       shinyjs::show("BoxResIntDistMeans")
       shinyjs::show("BoxResIntDistMedians")
+      shinyjs::show("title_activity_volume_metrics")
+      shinyjs::show("title_step_acc_metrics")
+      shinyjs::show("title_int_distri_metrics")
       } else {
       shinyjs::hide("myBox2")
       shinyjs::hide("Metric2")
@@ -5606,21 +5617,17 @@ app_server <- function(input, output, session) {
       shinyjs::hide("update_graphic2")
       shinyjs::hide("graph_int")
       shinyjs::hide("BoxResByDayVolTab")
-      shinyjs::hide("BoxResByDayVolFig")
       shinyjs::hide("BoxResVolMeans")
       shinyjs::hide("BoxResVolMedians")
-      shinyjs::hide("BoxCompaNormsFig")
       shinyjs::hide("BoxResByDayStepTab")
-      shinyjs::hide("BoxResByDayStepFig")
       shinyjs::hide("BoxResStepMeans")
       shinyjs::hide("BoxResStepMedians")
       shinyjs::hide("BoxResByDayIntDistTab")
-      shinyjs::hide("BoxResByDayIntDistFig1")
-      shinyjs::hide("BoxResByDayIntDistFig1Bis")
-      shinyjs::hide("BoxResByDayIntDistFig2")
-      shinyjs::hide("BoxSummaryIntDistFig")
       shinyjs::hide("BoxResIntDistMeans")
       shinyjs::hide("BoxResIntDistMedians")
+      shinyjs::hide("title_activity_volume_metrics")
+      shinyjs::hide("title_step_acc_metrics")
+      shinyjs::hide("title_int_distri_metrics")
       }
     })
     
@@ -6485,7 +6492,7 @@ app_server <- function(input, output, session) {
             cppac_diff_rasch = rasch_transform(x = sum(tab_cppac_summary_en()$"Difficulty score", na.rm = TRUE), quest = "C-PPAC", score = "difficulty"),
             cppac_amount_rasch = rasch_transform(x = sum(tab_cppac_summary_en()$"Amount score", na.rm = TRUE), quest = "C-PPAC", score = "quantity"),
             cppac_total_rasch = round((rasch_transform(x = sum(tab_cppac_summary_en()$"Difficulty score", na.rm = TRUE), quest = "C-PPAC", score = "difficulty") +
-                     rasch_transform(x = sum(tab_cppac_summary_en()$"Amount score", na.rm = TRUE), quest = "C-PPAC", score = "quantity")) / 2, 1),
+            rasch_transform(x = sum(tab_cppac_summary_en()$"Amount score", na.rm = TRUE), quest = "C-PPAC", score = "quantity")) / 2, 1),
             
             rendered_by_shiny = TRUE
           )
